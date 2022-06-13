f405d50b199bcfbcef9c4d14860701f7a127e712
Callum Hopkins <103919468@student.swin.edu.au>
2022-06-14 08:53:50 +1000

    Created files

---
+++ DiceTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiceTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
