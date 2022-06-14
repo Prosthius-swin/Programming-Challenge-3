38c7b2685f23619c85b086dfe1a43085f84fc740
Callum Hopkins <103919468@student.swin.edu.au>
2022-06-14 12:09:49 +1000

    Completed

--- DiceTask/Program.cs
+++ DiceTask/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
 namespace DiceTask
 {
@@ -6,7 +9,145 @@ namespace DiceTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int diceRollsTotal;
+            int dieSides;
+
+            string menuSelection = "";
+            string menu = "1. Roll dice \n2. Calculate average, total, or list all previous rolls \n3. Exit \n\n";
+            string lineSeparator = "-------------------------";
+            List<int> diceRolls = new List<int>();
+            ClGame C = new ClGame();
+
+            while(menuSelection != "3")
+            {
+                int counter = 0;
+                string rollAgain = "y";
+                int numberDiceRolls = 0;
+
+                Console.Write(menu);
+                menuSelection = Console.ReadLine();
+                Console.WriteLine(lineSeparator + "\n");
+                switch(menuSelection)
+                {
+                    //Roll dice
+                    case "1":
+                        while (rollAgain == "y" && numberDiceRolls <= 50 && numberDiceRolls >= 0) 
+                        {
+                            Console.WriteLine("Do you want to use a 6 sided die? y/n");
+                            string dieSidesResponse = Console.ReadLine().ToLower();
+                            if (dieSidesResponse == "y")
+                            {
+                                Die die = new Die();
+                                dieSides = 6;
+                            } else
+                            {
+                                Console.WriteLine("How many sides would you like?");
+                                dieSides = int.Parse(Console.ReadLine());
+                                Die die = new Die(dieSides);
+                            }
+
+                            Console.WriteLine("How many dice do you want to roll?\n");
+                            numberDiceRolls = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine(lineSeparator + "\n");
+        
+                            while (counter < numberDiceRolls) 
+                            {                                
+                                Game.AddDie(dieSides);
+                                counter++;
+                            }
+                            //Console.WriteLine("test");
+                            C.RollAllDice();
+                            foreach (int i in C.Results)
+                            {
+                                Console.Write($"{i} ");
+                            }
+
+                            Console.WriteLine("\n");
+                            Console.WriteLine("Would you like to roll again? (y/n)");
+                            rollAgain = Console.ReadLine().ToLower();
+                            if(rollAgain == "y")
+                            {
+                                counter = 0;
+                            } else
+                            {
+                                Console.WriteLine("\nSaving rolls");
+                            }
+                            Console.WriteLine("\n" + lineSeparator + "\n");   
+                        }
+                    break;
+
+                    //Calculate average, total, list the rolls
+                    case "2":
+                        string caseTwoChoice = "";
+
+                        while(caseTwoChoice != "D")
+                        {
+                            Console.WriteLine("A. Calculate average of rolls \nB. Calculate sum of rolls \nC. List all rolls \nD. Return to main menu \n");
+                            caseTwoChoice = Console.ReadLine().ToUpper();
+                            Console.WriteLine(lineSeparator);
+                            switch(caseTwoChoice)
+                            {
+                                //Calculate average of rolls
+                                case "A":
+                                    double average = C.GetAverage();
+                                    Console.WriteLine(average);
+                                    Console.WriteLine($"{lineSeparator}\n");
+                                    break;
+
+                                //Calculate sum of rolls
+                                case "B":
+                                    int total = C.GetTotal();
+                                    Console.WriteLine(total);
+                                    Console.WriteLine($"\n{lineSeparator}\n");
+                                    break;
+
+                                //List all rolls
+                                case "C":
+                                    PrintRolls(diceRolls);
+                                    Console.WriteLine($"\n\n{lineSeparator}\n");
+                                    break;
+
+                                //Return to main menu
+                                case "D":
+                                    Console.WriteLine($"\nReturning to main menu\n{lineSeparator}\n");
+                                break;
+                            }
+                        }
+                    break;
+
+                    //Exit
+                    case "3":
+                    Console.WriteLine("Exiting program");
+                    break;
+                }
+            }       
+        }
+
+        //Prints all numbers stored in the list 'diceRolls' to the console
+        private static void PrintRolls(List<int> diceRolls)
+        {
+            Console.WriteLine();
+            foreach (int i in diceRolls)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+
+        //Calculates the sum of all numbers stored in the list 'diceRolls' and prints to console
+        private static int DiceRollsSum(List<int> diceRolls)
+        {
+            int sumDiceRolls = diceRolls.Sum();
+            Console.WriteLine($"\nThe sum of all rolls is {sumDiceRolls}");
+            return sumDiceRolls;
+        }
+
+        //Calculates the average of numbers stored in the list 'diceRolls' and prints to console
+        private static double DiceRollsAverage(List<int> diceRolls)
+        {
+            double diceRollsAverage = diceRolls.Average();
+            Console.WriteLine($"The average of all rolls is {diceRollsAverage}\n");
+            return diceRollsAverage;
         }
     }
 }
--- DiceTaskUnitTest/UnitTest1.cs
+++ DiceTaskUnitTest/UnitTest1.cs
@@ -1,14 +1,122 @@
 using System;
 using Xunit;
+using DiceTask;
 
 namespace DiceTaskUnitTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(2, 7, 3, 11, 2, 2, 4.50)]
+        public void GetAverageTestOne(int input1,  int input2, int input3, int input4, int input5, int input6, int expected)
         {
+            ClGame C = new ClGame();
 
+            C.Results.Add(input1);
+            C.Results.Add(input2);
+            C.Results.Add(input3);
+            C.Results.Add(input4);
+            C.Results.Add(input5);
+            C.Results.Add(input6);
+
+            int total = C.GetTotal();
+            double result = total / C.Results.Count;
+            Assert.Equal(expected, result);
+        }
+        [Theory]
+        [InlineData(18, 4, 6, 12, 19, 11.80)]
+        public void GetAverageTestTwo(int input1,  int input2, int input3, int input4, int input5, double expected)
+        {
+            ClGame C = new ClGame();
+
+            C.Results.Add(input1);
+            C.Results.Add(input2);
+            C.Results.Add(input3);
+            C.Results.Add(input4);
+            C.Results.Add(input5);
+
+            double total = C.GetTotal();
+            double result = total / C.Results.Count;
+            Assert.Equal(expected, result);
+        }
+        [Theory]
+        [InlineData(5, 5)]
+        public void GetAverageTestThree(int input1, double expected)
+        {
+            ClGame C = new ClGame();
+
+            C.Results.Add(input1);
+
+            double total = C.GetTotal();
+            double result = total / C.Results.Count;
+            Assert.Equal(expected, result);
+        }
+        [Theory]
+        [InlineData(0, 0)]
+        public void GetAverageTestFour(int input1, double expected)
+        {
+            ClGame C = new ClGame();
+
+            C.Results.Add(input1);
+
+            double total = C.GetTotal();
+            double result = total / C.Results.Count;
+            Assert.Equal(expected, result);
+        }
+
+        
+        [Theory]
+        [InlineData(2, 7, 3, 11, 2, 2, 27)]
+        public void GetTotalTestOne(int input1,  int input2, int input3, int input4, int input5, int input6, int expected)
+        {
+            ClGame C = new ClGame();
+
+            C.Results.Add(input1);
+            C.Results.Add(input2);
+            C.Results.Add(input3);
+            C.Results.Add(input4);
+            C.Results.Add(input5);
+            C.Results.Add(input6);
+
+            int result = C.GetTotal();
+            Assert.Equal(expected, result);
+        }
+        [Theory]
+        [InlineData(18, 4, 6, 12, 19, 59)]
+        public void GetTotalTestTwo(int input1,  int input2, int input3, int input4, int input5, double expected)
+        {
+            ClGame C = new ClGame();
+
+            C.Results.Add(input1);
+            C.Results.Add(input2);
+            C.Results.Add(input3);
+            C.Results.Add(input4);
+            C.Results.Add(input5);
+
+            double result = C.GetTotal();
+            Assert.Equal(expected, result);
+        }
+        [Theory]
+        [InlineData(5, 5)]
+        public void GetTotalTestThree(int input1, double expected)
+        {
+            ClGame C = new ClGame();
+
+            C.Results.Add(input1);
+
+            double result = C.GetTotal();
+            Assert.Equal(expected, result);
+        }
+        [Theory]
+        [InlineData(0, 0)]
+        public void GetTotalTestFour(int input1, double expected)
+        {
+            ClGame C = new ClGame();
+
+            C.Results.Add(input1);
+
+            double result = C.GetTotal();
+            Assert.Equal(expected, result);
         }
     }
 }
